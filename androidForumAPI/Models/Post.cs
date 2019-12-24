628b58fa0f9418ce5dc4cb0c5ea684334498e22e
Sofie Seru <sofie.seru@student.hogent.be>
2019-12-24 14:49:17 +0100

    add post model

---
+++ androidForumAPI/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace androidForumAPI.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string PostTitle { get; set; }
+        public string PostDescription { get; set; }
+        public string CreatorName { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int LikesAmount { get; set; }
+        public int ReactionAmount { get; set; }
+        
+    }
+}
--- androidForumAPI/Startup.cs
+++ androidForumAPI/Startup.cs
@@ -25,7 +25,10 @@ namespace androidForumAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllers();
+            services.AddOpenApiDocument(); 
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +44,9 @@ namespace androidForumAPI
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwaggerUi3(); 
+            app.UseSwagger();
 
             app.UseEndpoints(endpoints =>
             {
